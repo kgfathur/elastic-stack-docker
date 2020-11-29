e301c513da36d48ef4058f5d3b9d0e0937e5151a
kgfathur <sahabatsolder@gmail.com>
2020-11-29 17:17:06 +0700

    add clean script

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo rm -rf /data/es01/*
+sudo rm -rf /data/es02/*
+sudo rm -rf /data/es03/*
+sudo rm -rf /data/es01/.*
+sudo rm -rf /data/es02/.*
+sudo rm -rf /data/es03/.*
+sudo rm -rf /data/kibana/*
+sudo rm -rf /data/kibana/.*
