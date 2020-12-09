a51ec9841316bbdd9e12bd3ae12908ebb78024cb
kgfathur <sahabatsolder@gmail.com>
2020-12-09 17:33:05 +0700

    check env

--- es-setup/app.py
+++ es-setup/app.py
@@ -24,7 +24,8 @@ passwords = {}
 if (set_passwd == True or set_passwd == 'True' or set_passwd == 'true' or set_passwd == 'TRUE'):
     if (set_master == True or set_master == 'True' or set_master == 'true' or set_master == 'TRUE'):
         print('Set using master password')
-        if (set_master_password != None or set_master_password == ''):
+        print('master_password [{}]'.format(set_master_password))
+        if (set_master_password != None or set_master_password != ''):
             passwords['elastic'] = set_master_password
             passwords['apm_system'] = set_master_password
             passwords['kibana'] = set_master_password
