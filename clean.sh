8457b30409758c0171ab620ad79cabc21fb9a6f3
kgfathur <sahabatsolder@gmail.com>
2021-01-13 00:02:31 +0700

    renew clean script

--- clean.sh
+++ clean.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-sudo rm -rf /data/es01/*
-sudo rm -rf /data/es02/*
-sudo rm -rf /data/es03/*
-sudo rm -rf /data/es01/.*
-sudo rm -rf /data/es02/.*
-sudo rm -rf /data/es03/.*
-sudo rm -rf /data/kibana/*
-sudo rm -rf /data/kibana/.*
+sudo rm -vrf /data/es0{1..3}/*
+sudo rm -vrf /data/es0{1..3}/.[^\.][a-zA-Z]*
+
+sudo rm -vrf /data/log_es0{1..3}/*
+sudo rm -vrf /data/log_es0{1..3}/.[^\.][a-zA-Z]*
+
+sudo rm -vrf /data/prometheus{0..2}/*
+sudo rm -vrf /data/prometheus{0..2}/.[^\.][a-zA-Z]*
+
+sudo rm -vrf /data/sidecar{0..2}/*
+sudo rm -vrf /data/sidecar{0..2}/.[^\.][a-zA-Z]*
+
+sudo rm -vrf /data/kibana/*
+sudo rm -vrf /data/kibana/.[^\.][a-zA-Z]*
+
+sudo ls -lRa /data
