57410386047dd822107a275eb2531e8529286b5a
kgfathur <sahabatsolder@gmail.com>
2021-02-08 00:27:43 +0700

    dev mode running

--- clean.sh
+++ clean.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-sudo rm -vrf /data/es0{1..3}/*
-sudo rm -vrf /data/es0{1..3}/.[^\.][a-zA-Z]*
+sudo rm -vrf /data/es-master{1..3}/*
+sudo rm -vrf /data/es-master{1..3}/.[^\.][a-zA-Z]*
 
-sudo rm -vrf /data/log_es0{1..3}/*
-sudo rm -vrf /data/log_es0{1..3}/.[^\.][a-zA-Z]*
+sudo rm -vrf /data/es-data{1..3}/*
+sudo rm -vrf /data/es-data{1..3}/.[^\.][a-zA-Z]*
 
-sudo rm -vrf /data/prometheus{0..2}/*
-sudo rm -vrf /data/prometheus{0..2}/.[^\.][a-zA-Z]*
+# sudo rm -vrf /data/prometheus{0..2}/*
+# sudo rm -vrf /data/prometheus{0..2}/.[^\.][a-zA-Z]*
 
-sudo rm -vrf /data/sidecar{0..2}/*
-sudo rm -vrf /data/sidecar{0..2}/.[^\.][a-zA-Z]*
+# sudo rm -vrf /data/sidecar{0..2}/*
+# sudo rm -vrf /data/sidecar{0..2}/.[^\.][a-zA-Z]*
 
-sudo rm -vrf /data/kibana/*
-sudo rm -vrf /data/kibana/.[^\.][a-zA-Z]*
+# sudo rm -vrf /data/kibana/*
+# sudo rm -vrf /data/kibana/.[^\.][a-zA-Z]*
 
-sudo rm -vrf /data/grafana/*
-sudo rm -vrf /data/grafana/.[^\.][a-zA-Z]*
+# sudo rm -vrf /data/grafana/*
+# sudo rm -vrf /data/grafana/.[^\.][a-zA-Z]*
 
 sudo ls -lRa /data
